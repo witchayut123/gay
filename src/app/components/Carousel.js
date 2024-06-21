b35f8f6f3945152c19d57b6562296b4db42e1dc8
unknown <orange>
2024-06-21 13:00:06 +0700

    first commit

---
+++ src/app/components/Carousel.js
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import Slider1 from "/public/Slider1.jpg"
+import Slider2 from "/public/Slider2.jpg"
+export default function Carousel() {
+return (
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+            <div className="carousel-item active">
+                <Image src= {Slider1} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+                <Image src= {Slider2} className="d-block w-100" alt="..." />
+            </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+        </button>
+        </div>
+);
+}
---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+    <h1><center>Contact Page</center></h1>
+    <br />
+    <center><Link href="/">Home</Link> | <Link href="/about">About</Link> | <Link href="/service">Service</Link> | <Link href="/contact">Contact</Link></center>
+    </>
+  );
+}
